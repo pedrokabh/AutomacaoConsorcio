6444458d6d966eb929568eede678c8a65157cb3f
Pedro Lucas <102974185+pedrokabh@users.noreply.github.com>
2024-06-13 20:36:37 -0300

    V1.0.1
    Calculo de Taxas ao mes e ao ano

--- ConsorcioBB.py
+++ ConsorcioBB.py
@@ -1,7 +1,3 @@
-# -- ANOTAÇÕES -- #
-# 1.0 Testes Realizados com Chrome Driver v 125.0.6422.141 - https://googlechromelabs.github.io/chrome-for-testing/#stable
-# 1.1 Testes Realizados com Selenium v 4.21.0
-
 try:
     # Importações Selenium
     from selenium import webdriver
@@ -14,39 +10,41 @@ try:
     import pandas as pd
     # Importações padrões
     import time
-    import os
+    import os, sys
 except Exception as err:
-    print(f"[ERROR] \n{err}")
+    print(f"[ConsorcioBB] FALHA AO CARREGAR IMPORTAÇÕES \n{err}")
+    sys.exit(1)
 
 class ConsorcioBB:
-    def __init__(self, login, senha):
+    def __init__(self, login, senha, endereçoChromeDriver):
 
-        # -- fluxo de execução --#
+        # 1.0 # VERIFICANDO VERSÃO SELENIUM
         os.system('cls')
-        print("Sistema Rodando Selenium Versão (" + webdriver.__version__ + ").")
+        print("[ConsorcioBB] Versão Selenium Em Execução (V" + webdriver.__version__ + ").")
+        # 1.0 #
         
+        # 1.1 # INICIANDO NAVEGADOR, FAZENDO LOGON E FECHANDO POUP-UP.
         self.login = login
         self.senha = senha
-        self.chrome_driver_path = r"C:\Users\pedro\OneDrive\Área de Trabalho\AUTOMAÇÕES ISF\chromedriver-win64\chromedriver.exe"
-
+        self.chrome_driver_path = endereçoChromeDriver
         self.iniciar_navegador()
         self.fazer_login()
         self.fechar_popup()
+        # 1.1 #
 
     # --- FUNÇÕES PARA GERAR GRUPOS ATIVOS --- #
-  
     def generate_dataFrame_gruposAtivos(self, sigla):
         try:
             grupos_ativos = []
-            self.browser.execute_script("document.getElementById('ctl00_Conteudo_rptFormularios_ctl08_lnkFormulario').click();") # Clicando em simulador de contratação
+            self.browser.execute_script("document.getElementById('ctl00_Conteudo_rptFormularios_ctl08_lnkFormulario').click();")
             time.sleep(3)
 
-            if sigla == "IM240": # Grupos IMOVEIS 240
+            if sigla == "IM240":
                 self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_Conteudo_cbxTipoGrupo"]/option[3]'))).click()
                 time.sleep(2)
                 self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_Conteudo_cbxTipoVenda"]'))).click()
                 self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_Conteudo_cbxTipoVenda"]/option[2]'))).click()
-            elif sigla == "IMP": # Grupos IMOVEIS PADROES
+            elif sigla == "IMP":
                 self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_Conteudo_cbxTipoGrupo"]/option[3]'))).click()
                 time.sleep(2)
                 self.wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="ctl00_Conteudo_cbxTipoVenda"]'))).click()
@@ -54,13 +52,13 @@ class ConsorcioBB:
             else:
                 self.browser.execute_script(f"document.querySelector('#ctl00_Conteudo_cbxTipoGrupo option[value=\"{sigla}\"]').selected = true")
             
-            self.browser.execute_script("document.getElementById('ctl00_Conteudo_lnkProximo').click();") # Pesquisar categoria
+            self.browser.execute_script("document.getElementById('ctl00_Conteudo_lnkProximo').click();")
             cod = 1
 
             while cod != 0:
                 time.sleep(2)
-                tabela = self.browser.find_element(By.XPATH, '//*[@id="ctl00_Conteudo_grdGruposDisponiveis"]')  # Localizar a tabela pelo ID
-                linhas = tabela.find_elements(By.TAG_NAME, 'tr')  # Encontrar todas as linhas da tabela, ignorando o cabeçalho
+                tabela = self.browser.find_element(By.XPATH, '//*[@id="ctl00_Conteudo_grdGruposDisponiveis"]')
+                linhas = tabela.find_elements(By.TAG_NAME, 'tr')
                 just_one_table = True
 
                 for linha in linhas:
@@ -74,49 +72,52 @@ class ConsorcioBB:
                     elif linha.find_elements(By.XPATH,'.//input[contains(@src, "back.png")]'):
                         cod = 0
                         break
-                    elif len(linha.find_elements(By.TAG_NAME, 'td'))>3:
+                    elif len(linha.find_elements(By.TAG_NAME, 'td')) > 3:
                         celulas = linha.find_elements(By.TAG_NAME, 'td')  # Encontrar todas as células da linha
 
                         codigo_grupo = celulas[0].text
-                        prazo = celulas[1].text
-                        vagas = celulas[2].text
-                        txAdm = celulas[4].text
-                        fReserv = celulas[5].text
-                        valorBem = celulas[7].text
+                        prazo = int(celulas[1].text)
+                        vagas = int(celulas[2].text)
+                        txAdm = float(celulas[4].text.replace(',', '.'))  # Converte string para float
+                        fReserv = float(celulas[5].text.replace(',', '.'))  # Converte string para float
+                        valorBem = float(celulas[7].text.replace('.', '').replace(',', '.'))  # Converte string para float
+
+                        # Calculando novas colunas
+                        txAdmFr = f"{txAdm:.2f}% + {fReserv:.2f}%"
+                        calcTxAdmMensal = f"{(txAdm / prazo):.3f}% a.m\n{(txAdm / prazo * 12):.3f}% a.a"
+                        calcFRMensal = f"{(fReserv / prazo):.3f}% a.m\n{(fReserv / prazo * 12):.3f}% a.a"
+                        calcTotalMensal = f"{((txAdm + fReserv) / prazo):.3f}% a.m\n{((txAdm + fReserv) / prazo * 12):.3f}% a.a"
 
                         cartas_Grupo = {
                             "TipoGrupo": self.traduzir_sigla(sigla),
-                            "Codigo": int(codigo_grupo[2:]), # Tirar 2 primeiros caracter
-                            "Prazo": int(prazo),
-                            "Vagas": int(vagas),
-                            "TaxaAdm": f"{float(txAdm.replace(',', '.')):.2f}%",
-                            "FReserva": f"{float(fReserv.replace(',', '.')):.2f}%",
-                            "CartasCredito": float(valorBem.replace('.', '').replace(',', '.'))
+                            "Codigo": int(codigo_grupo[2:]),  # Tirar 2 primeiros caracteres
+                            "Prazo": prazo,
+                            "Vagas": vagas,
+                            "TxAdm+FR": txAdmFr,
+                            "CalcTxAdm": calcTxAdmMensal,
+                            "CalcFR": calcFRMensal,
+                            "CalcTotal": calcTotalMensal,
+                            "CartasCredito": valorBem
                         }
                         grupos_ativos.append(cartas_Grupo)
+
                 
-                # Verifica se tabela tem apenas uma página para encerrar metodo.
                 if just_one_table == True:
-                    self.browser.find_element(By.XPATH,'//*[@id="ctl00_lnkHome"]').click() # Volta para menu principal.
-                    df = pd.DataFrame(grupos_ativos)
-                    df = df.loc[df.groupby(['Codigo', 'Prazo'])['CartasCredito'].idxmin()] # Filtrando apenas menor carta
-                    return df # Retorna df com grupos ativos da sigla.
-                
-            self.browser.find_element(By.XPATH,'//*[@id="ctl00_lnkHome"]').click() # Volta para menu principal.
+                    cod = 0
+            
+            self.browser.find_element(By.XPATH,'//*[@id="ctl00_lnkHome"]').click()
             df = pd.DataFrame(grupos_ativos)
-            df = df.loc[df.groupby(['Codigo', 'Prazo'])['CartasCredito'].idxmin()] # Filtrando apenas menor carta
-            return df # Retorna df com grupos ativos da sigla.
-                
+            df = df.loc[df.groupby(['Codigo', 'Prazo'])['CartasCredito'].idxmin()]
+            return df      
         except Exception as err:
-            print(f"[ERROR] Falha ao levantar grupos ativos. \n {err}")
+            print(f"[ConsorcioBB] Falha no metodo generate_dataFrame_gruposAtivos. \n {err}")
+            sys.exit(1)
 
     # --- FUNÇÕES PARA GERAR DADOS ASSEMBLEIAS --- #
     def generate_dataFrame_dadosAssembleia(self, lista_grupos):
         try:
-            print("\nEXTRAINDO DADOS DA ASSEMBLEIA")
             time.sleep(3)
-            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_rptFormularios_ctl27_lnkFormulario"]'))).click() # Clicando na página da assembleia
-            
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_rptFormularios_ctl27_lnkFormulario"]'))).click()
             dados_grupos = []
 
             for grupo in lista_grupos:
@@ -129,42 +130,36 @@ class ConsorcioBB:
                 }
 
                 script = f"document.evaluate('//*[@id=\"ctl00_Conteudo_edtCD_Grupo\"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '{str(grupo).zfill(6)}';"
-                self.browser.execute_script(script) # Alterando valor do grupo
+                self.browser.execute_script(script)
                 script = "document.getElementById('ctl00_Conteudo_lnkPesquisar').click();"
-                self.browser.execute_script(script) # Clicando em pesquisar
+                self.browser.execute_script(script)
 
-                script = "return document.getElementById('ctl00_Conteudo_lblVolumeTotal').textContent;"# Script para pegar Volume total
-                volume_total_label = self.browser.execute_script(script) # Executar o script usando execute_script
+                script = "return document.getElementById('ctl00_Conteudo_lblVolumeTotal').textContent;"
+                volume_total_label = self.browser.execute_script(script)
                 dados_assembleias_grupo["VolumeTotal"] = volume_total_label
 
-                tabela = self.browser.find_element(By.ID, 'ctl00_Conteudo_grdAssembleias')# Localizar a tabela pelo ID
-                linhas = tabela.find_elements(By.TAG_NAME, 'tr')[1:] # Encontrar todas as linhas da tabela, ignorando o cabeçalho
+                tabela = self.browser.find_element(By.ID, 'ctl00_Conteudo_grdAssembleias')
+                linhas = tabela.find_elements(By.TAG_NAME, 'tr')[1:]
 
-                # Iterar sobre as linhas e extrair os valores das primeiras tds de cada linha
                 for linha in linhas:
-                    celulas = linha.find_elements(By.TAG_NAME, 'td') # Encontrar todas as células da linha
+                    celulas = linha.find_elements(By.TAG_NAME, 'td')
 
                     assembleia = celulas[1].text
                     data = celulas[2].text
                     qtde_contemp = celulas[3].text
-                    # maior_lance = celulas[4].text
                     menor_lance = celulas[5].text
 
-                    # Adiciona os dados à estrutura do grupo
                     if data == '27/05/2024':
                         dados_assembleias_grupo["A 05/24"] = assembleia
                         dados_assembleias_grupo["QtdCont 05/24"] = qtde_contemp
-                        # dados_assembleias_grupo["MAL 05/24"] = "{:.2f}%".format(float(maior_lance.replace(",", ".")))
                         dados_assembleias_grupo["MEL 05/24"] = "{:.2f}%".format(float(menor_lance.replace(",", ".")))
                     elif data == '25/04/2024':
                         dados_assembleias_grupo["A 04/24"] = assembleia
                         dados_assembleias_grupo["QtdCont 04/24"] = qtde_contemp
-                        # dados_assembleias_grupo["MAL 04/24"] = "{:.2f}%".format(float(maior_lance.replace(",", ".")))
                         dados_assembleias_grupo["MEL 04/24"] = "{:.2f}%".format(float(menor_lance.replace(",", ".")))
                     elif data == '25/03/2024':
                         dados_assembleias_grupo["A 03/24"] = assembleia
                         dados_assembleias_grupo["QtdCont 03/24"] = qtde_contemp
-                        # dados_assembleias_grupo["MAL 03/24"] = "{:.2f}%".format(float(maior_lance.replace(",", ".")))
                         dados_assembleias_grupo["MEL 03/24"] = "{:.2f}%".format(float(menor_lance.replace(",", ".")))
                 
                 dados_grupos.append(dados_assembleias_grupo)
@@ -172,14 +167,15 @@ class ConsorcioBB:
             df = pd.DataFrame(dados_grupos)
             return df
         except Exception as err:
-            print(f"\n[ERROR] Falha ao extrair dados da assembleia.\n{err}")
+            print(f"\n[ConsorcioBB] Falha no Metodo generate_dataFrame_dadosAssembleia.\n{err}")
+            sys.exit(1)
 
     # --- FUNÇÕES COMPLEMENTARES ---#
     def traduzir_sigla(self, sigla):
         dicionario_siglas = {
             "MO": "MOTO DEMAIS",
-            "EE": "OUTROS BENS MOVEIS",
-            "TC": "TRATOR E CAMINHAO GERAL",
+            "EE": "BENS MOVEIS",
+            "TC": "TRATOR E CAMINHAO",
             "IM240": "IMOVEIS 240",
             "IMP": "IMOVEIS PADRAO",
             "AI": "AUTO IPCA",
@@ -190,39 +186,44 @@ class ConsorcioBB:
     # --- FUNÇÕES PADRÕES -- #
     def iniciar_navegador(self):
         try:
-            print("INICIANDO NAVEGADOR CHROME")
+            print("\n[ConsorcioBB] Iniciando Chrome...")
             chrome_service = Service(self.chrome_driver_path)
             self.browser = webdriver.Chrome(service=chrome_service)
             self.wait = WebDriverWait(self.browser, 10) 
-            print("NAVEGADOR INICIADO COM SUCESSO!\nACESSANDO PORTAL PARCEIROS BB CONSÓRCIO (www.parceirosbbconsorcios.com.br)")
-            self.browser.get('https://www.parceirosbbconsorcios.com.br/')
         except Exception as err:
-            print(f"\n[ERROR] Falha ao inicializar navegador.\n{err}")
+            print(f"\n[ConsorcioBB] Falha no método. iniciar_navegador.\n{err}")
+            sys.exit(1)
     
     def fazer_login(self):
         try:
-            print("REALIZANDO LOGIN NO PORTAL PARCEIROS BB")
+            print("\n[ConsorcioBB] Acessando URL (https://www.parceirosbbconsorcios.com.br).")
+            self.browser.get('https://www.parceirosbbconsorcios.com.br/')
+            print("[ConsorcioBB] Efetuando login PP BB Consórcio.")
             self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_edtUsuario"]'))).send_keys(self.login)
             self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_edtSenha"]'))).send_keys(self.senha)
             self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_lnkEntrar"]'))).click()
-            print("LOGON EFETUADO COM SUCESSO.")
+            print("[ConsorcioBB] Logado com sucesso no PP BB Consórcio.")
         except Exception as err:
-            print(f"\n[ERROR] Falha ao realizar login.\n{err}")
+            print(f"\n[ConsorcioBB] Falha no metodo fazer_login.\n{err}")
+            sys.exit(1)
     
     def fechar_popup(self):
         try:
-            print('FECHANDO POUP-UP')
+            print('[ConsorcioBB] Fechando Poup-Up Inicial.')
             self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_Conteudo_LinkButton1"]'))).click()
-            print("POUP-UP FECHADO")
+            print("[ConsorcioBB] Poup-Up fechado.")
         except Exception as err:
-            print(f"\n[ERROR] Falha ao fechar poup-Up pós login.\n{err}")
-            return
+            print(f"\n[ConsorcioBB] Falha no metodo fechar_popUp.\n{err}")
+            sys.exit(1)
     
     def encerrar_navegador(self):
         try:
             # Encerra navegador.
+            print("[ConsorcioBB] Encerrando Chrome.")
             self.browser.quit()
+            sys.exit(0)
         except Exception as err:
-            print(f"\n[ERROR] Falha ao encerrar browser.\n{err}")
+            print(f"\n[ConsorcioBB] Falha no método encerrar_navegador.\n{err}")
+            sys.exit(1)
 
 
