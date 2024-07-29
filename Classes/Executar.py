708646265f240122762772625ee155f22c4234b5
Pedro Câmara <pedrocamarabh@gmail.com>
2024-07-29 15:11:53 -0300

    update structure

--- Classes/Executar.py
+++ Classes/Executar.py
@@ -5,44 +5,47 @@ from datetime import datetime
 from ConsorcioBB import ConsorcioBB
 from Logger import Logger
 
-# LENDO LOG COUNT.TXT
-with open(str(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))+'\\Classes\\execution_count.txt'), 'r') as arquivo:
-    conteudo = arquivo.read()
+try:
+    
+    # LENDO LOG COUNT.TXT
+    with open(str(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))+'\\Classes\\execution_count.txt'), 'r') as arquivo:
+        conteudo = arquivo.read()
 
-"""
-    !!! ATENÇÃO !!! - Caso seja outra assembleia, atualizar as colunas do mês.
-"""
-# !--- PARAMETROS DE EXECUÇÃO ---! #
-execution_code = int(conteudo)
-parte2 =True
-parte1 = True
-media_assembleia = True
-categoria_processadas = ["TC", "AI", "AU", "MO", "EE", "IM240", "IMP"]
-endereco_chrome_driver = r"..\chromedriver-win64\chromedriver.exe"
-data_assembleia_mais_recente = "26/07/2024" # JULHO
-data_assembleia_passada = "25/06/2024" # JUNHO
-data_assembleia_retrasada = "27/05/2024" # MAIO
-login = input("Digite o seu login: ")
-senha = input("Digite a sua senha: ")
-# !--- PARAMETROS DE EXECUÇÃO ---! #
+    """
+        !!! ATENÇÃO !!! - Caso seja outra assembleia, atualizar as colunas do mês.
+        !!! ATENÇÃO !!! - Caso seja outra assembleia, deve-se extrair as vagas dos grupos na assembleia anterior e gerar o arquivo VagasMX.xlsxx
+        E modificar as colunas na mão.
+    """
+    # !--- PARAMETROS DE EXECUÇÃO ---! #
+    execution_code = int(conteudo)
+    parte1 = True
+    parte2 =True # TODA NOVA ASSEMBLEIA 
+    media_assembleia = True
+    categoria_processadas = ["TC", "AI", "AU", "MO", "EE", "IM240", "IMP"]
+    endereco_chrome_driver = r"..\chromedriver-win64\chromedriver.exe"
+    data_assembleia_mais_recente = "26/07/2024" # JULHO
+    data_assembleia_passada = "25/06/2024" # JUNHO 
+    data_assembleia_retrasada = "27/05/2024" # MAIO
+    login = input("Digite o seu login: ")
+    senha = input("Digite a sua senha: ")
+    # !--- PARAMETROS DE EXECUÇÃO ---! #
 
 
-"""
-    GERA ARQUIVO TODOSGRUPOSATIVOS.XLSX (DADOS DOS GRUPOS / MEDIA CONTEMPLAÇÃO / DADOS ASSEMBLEIAS)
-"""
-if parte1:
-    try:
-        os.system('cls')
-
-        # DIRETORIO ARQUIVO LOG.
-        directory_mother = f'{os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))}\\Builds\\Build Number {execution_code}\\'
-        os.makedirs(os.path.dirname( str(directory_mother) ), exist_ok=True)
-
+    # INICIALIZANDO LOG
+    logger = Logger(execution_code, write_log_directory=f'.\\Builds\\Build Number {execution_code}\\Build {execution_code}.log')
+    datetime_start_execution = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    logger.info(f"[Executar] Iniciando execução [{execution_code}] datetime [{datetime_start_execution}]")
+    
+    # GERA ARQUIVO TODOSGRUPOSATIVOS.XLSX (DADOS DOS GRUPOS / MEDIA CONTEMPLAÇÃO / DADOS ASSEMBLEIAS)
+    if parte1:
         try:
-            # INICIALIZANDO LOG
-            logger = Logger(execution_code, write_log_directory=f'.\\Builds\\Build Number {execution_code}\\Build {execution_code}.log')
-            datetime_start_execution = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            logger.info(f"[Executar] Iniciando execução [{execution_code}] datetime [{datetime_start_execution}]")
+            os.system('cls')
+
+            # DIRETORIO ARQUIVO LOG.
+            directory_mother = f'{os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))}\\Builds\\Build Number {execution_code}\\'
+            os.makedirs(os.path.dirname( str(directory_mother) ), exist_ok=True)
+
+            
 
             ## --- Execução --- ##
             consorcio = ConsorcioBB(
@@ -100,66 +103,60 @@ if parte1:
             df_excelFinal = df_excelFinal[colunas]
             df_excelFinal.to_excel(f'.\\Builds\\Build Number {execution_code}\\TodosGruposAtivos.xlsx', index=False)
             logger.info("[Executar] Arquivo 'TodosGruposAtivos.xlsx' criado com sucesso.")
-            consorcio.EndBrowser()
-
-            # ATUALIZANDO LOG COUNT.TXT
-            with open(str(directory_mother+'\\Classes\\execution_count.txt'), 'w') as arquivo:
-                arquivo.write(str(execution_code + 1))
-            datetime_end_execution = datetime.now().strftime("%H:%M:%S")
-            logger.info(f"[Executar] Execução Finalizada [{datetime_end_execution}] | new execution count [{execution_code + 1}]")
-
+            consorcio.EndBrowser()   
         except Exception as err:
-            if 'logger' in locals() and hasattr(logger, 'error'):
-                logger.error(f"\n[Executar] Falha ao executar programa.\nERROR-> {err}")
-            datetime_end_execution = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            execution_code += 1
-            if 'logger' in locals() and hasattr(logger, 'error'):
-                logger.error(f"[Executar] Execução Finalizada [{datetime_end_execution}] | new execution count [{execution_code}]")
+            print(f"[Executar] FALHA AO CARREGAR IMPORTAÇÕES \n{err}")
             sys.exit(1)
-    except Exception as err:
-        print(f"[Executar] FALHA AO CARREGAR IMPORTAÇÕES \n{err}")
-        sys.exit(1)
 
+    #ADICIONA QUANTIDADE DE VENDAS NO ARQUIVO E GERA O 'TODOSGRUPOSATIVOSCOMVENDAS.XLSX'
+    if parte2:
+        try:
+            # Ler os arquivos Excel
+            todos_grupos_ativos = pd.read_excel(
+                f'.\\Builds\\Build Number {execution_code}\\TodosGruposAtivos.xlsx',
+                sheet_name='Sheet1')
+            vagasM5 = pd.read_excel(r'.\\Classes\\VagasM5.xlsx', sheet_name='Planilha1')
+            vagasM6 = pd.read_excel(r'.\\Classes\\VagasM6.xlsx', sheet_name='Planilha1')
+            vagasM7 = pd.read_excel(r'.\\Classes\\VagasM7.xlsx', sheet_name='Planilha1')
 
-"""
-    ADICIONA QUANTIDADE DE VENDAS NO ARQUIVO E GERA O 'TODOSGRUPOSATIVOSCOMVENDAS.XLSX'
-"""
-if parte2:
-    try:
-        # Ler os arquivos Excel
-        todos_grupos_ativos = pd.read_excel(
-            f'.\\Builds\\Build Number {execution_code}\\TodosGruposAtivos.xlsx',
-            sheet_name='Sheet1')
-        vagasM5 = pd.read_excel(r'.\\Classes\\VagasM5.xlsx', sheet_name='Planilha1')
-        vagasM6 = pd.read_excel(r'.\\Classes\\VagasM6.xlsx', sheet_name='Planilha1')
-        vagasM7 = pd.read_excel(r'.\\Classes\\VagasM7.xlsx', sheet_name='Planilha1')
+            # Selecionar as colunas relevantes dos arquivos de vagas
+            vagasM5 = vagasM5[['Grupo', 'Vagas M5']]
+            vagasM6 = vagasM6[['Grupo', 'Vagas M6']]
+            vagasM7 = vagasM7[['Grupo', 'Vagas M7']]
 
-        # Selecionar as colunas relevantes dos arquivos de vagas
-        vagasM5 = vagasM5[['Grupo', 'Vagas M5']]
-        vagasM6 = vagasM6[['Grupo', 'Vagas M6']]
-        vagasM7 = vagasM7[['Grupo', 'Vagas M7']]
+            # Mesclar os dados com base na coluna 'Grupo'
+            merged_data = pd.merge(todos_grupos_ativos, vagasM5, on='Grupo', how='left')
+            merged_data = pd.merge(merged_data, vagasM6, on='Grupo', how='left')
+            merged_data = pd.merge(merged_data, vagasM7, on='Grupo', how='left')
 
-        # Mesclar os dados com base na coluna 'Grupo'
-        merged_data = pd.merge(todos_grupos_ativos, vagasM5, on='Grupo', how='left')
-        merged_data = pd.merge(merged_data, vagasM6, on='Grupo', how='left')
-        merged_data = pd.merge(merged_data, vagasM7, on='Grupo', how='left')
+            # Garantir que os valores ausentes fiquem em branco
+            merged_data['Vagas M5'] = pd.to_numeric(merged_data['Vagas M5'], errors='coerce').fillna(0)
+            merged_data['Vagas M6'] = pd.to_numeric(merged_data['Vagas M6'], errors='coerce').fillna(0)
+            merged_data['Vagas M7'] = pd.to_numeric(merged_data['Vagas M7'], errors='coerce').fillna(0)
+            merged_data['Vagas'] = pd.to_numeric(merged_data['Vagas'], errors='coerce').fillna(0)
 
-        # Garantir que os valores ausentes fiquem em branco
-        merged_data['Vagas M5'] = pd.to_numeric(merged_data['Vagas M5'], errors='coerce').fillna(0)
-        merged_data['Vagas M6'] = pd.to_numeric(merged_data['Vagas M6'], errors='coerce').fillna(0)
-        merged_data['Vagas M7'] = pd.to_numeric(merged_data['Vagas M7'], errors='coerce').fillna(0)
-        merged_data['Vagas'] = pd.to_numeric(merged_data['Vagas'], errors='coerce').fillna(0)
+            # Calcular as colunas 'Vendas M6', 'Vendas M7' e 'Vendas Atuais'
+            merged_data['Vendas M6'] = merged_data['Vagas M5'] - merged_data['Vagas M6']
+            merged_data['Vendas M7'] = merged_data['Vagas M6'] - merged_data['Vagas M7']
+            merged_data['Vendas Atuais'] = merged_data['Vagas M7'] - merged_data['Vagas']
+            # merged_data = merged_data.drop(columns=['Vagas M5', 'Vagas M6', 'Vagas M7'])
 
-        # Calcular as colunas 'Vendas M6', 'Vendas M7' e 'Vendas Atuais'
-        merged_data['Vendas M6'] = merged_data['Vagas M5'] - merged_data['Vagas M6']
-        merged_data['Vendas M7'] = merged_data['Vagas M6'] - merged_data['Vagas M7']
-        merged_data['Vendas Atuais'] = merged_data['Vagas M7'] - merged_data['Vagas']
-        # merged_data = merged_data.drop(columns=['Vagas M5', 'Vagas M6', 'Vagas M7'])
+            # Salvar o novo arquivo atualizado
+            merged_data.to_excel(f'.\\Builds\\Build Number {execution_code}\\TodosGruposAtivosComVendas.xlsx', index=False)
+            logger.warning(f"[Executar] ARQUIVO 'TodosGruposAtivosComVendas.xlsx' GERADO COM SUCESSO.")
+        except Exception as err:
+            print(err)
 
-        # Salvar o novo arquivo atualizado
-        merged_data.to_excel(f'.\\Builds\\Build Number {execution_code}\\TodosGruposAtivosComVendas.xlsx', index=False)
-        print("fim")
+    # ATUALIZANDO LOG COUNT.TXT
+    with open(str(directory_mother+'\\Classes\\execution_count.txt'), 'w') as arquivo:
+        arquivo.write(str(execution_code + 1))
+    logger.info(f"[Executar] Execução Finalizada [{datetime.now().strftime("%H:%M:%S")}] | new execution count [{execution_code + 1}]")    
 
-    except Exception as err:
-        print(err)
-
+except Exception as err:
+    if 'logger' in locals() and hasattr(logger, 'error'):
+        logger.error(f"\n[Executar] Falha ao executar programa.\nERROR-> {err}")
+    datetime_end_execution = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    execution_code += 1
+    if 'logger' in locals() and hasattr(logger, 'error'):
+        logger.error(f"[Executar] Execução Finalizada [{datetime_end_execution}] | new execution count [{execution_code}]")
+    sys.exit(1)
